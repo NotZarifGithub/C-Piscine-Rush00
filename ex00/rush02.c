47e9abb6adcb9327c9b8df1b37d8cb8fd9de3aa8
Muhammad Zarif Abd Raman <mabd-ram@u81z09s06.42kl.edu.my>
2024-04-27 21:28:37 +0800

    rush02 and rush 03 completed

---
+++ ex00/rush02.c
@@ -0,0 +1,26 @@
+void ft_putchar(char c);
+
+void rush(int x, int y)
+{
+	int row, col;
+
+	row = 1;
+	while (row <= y)
+	{
+		col = 1;
+		while (col <= x)
+		{
+			if ((row == 1 && col == 1) || (row == 1 && col == x))
+				ft_putchar('A');
+			else if ((row == y && col == 1) || (row == y && col == x))
+				ft_putchar('C');
+			else if (row == 1 || row == x || col == 1 || col == x)
+				ft_putchar('B');
+			else 
+				ft_putchar(' ');
+			col++;
+		}
+		ft_putchar('\n');
+		row++;
+	}
+}
